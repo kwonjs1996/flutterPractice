c4537c6392be5d4cebcda58c2ac2a3369ff7cbbc
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-03 19:44:11 +0900

    StatefulWidget, StateLessWidget

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class  extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
