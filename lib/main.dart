2c3c334612bdaee0c8edf751cd77c0f4a622336d
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-04 18:33:42 +0900

    assets, SizedBox, Divider(=<br/>)

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,135 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
-class  extends StatefulWidget {
-  const ({Key? key}) : super(key: key);
-
-  @override
-  State<> createState() => _State();
-}
-
-class _State extends State<> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/1.jpg'),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'BBANTO',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'using lightsaber',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'face hero tottoo',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'fire flames',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1),
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/2.jpg'),
+                radius: 40,
+                backgroundColor: Colors.amber[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
