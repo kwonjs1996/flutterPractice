96356c63727a5d0eaa287f632fd60e8ca753b78e
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-06 19:35:15 +0900

    reading, actions

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,26 @@ class MyPage extends StatelessWidget {
         title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0,
+        // leading -> 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치할 때 사용
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {
             print('menu button is clicked');
           },
         ),
+        // actions -> 복수의 아이콘 버튼 등을 오른쪽에 배치할 때 사용
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('shopping_cart button is clicked');
+              },
+              icon: Icon(Icons.shopping_cart)),
+          IconButton(
+              onPressed: () {
+                print('search button is clicked');
+              },
+              icon: Icon(Icons.search))
+        ],
       ),
     );
   }
