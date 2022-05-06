d93bad4f3c3c46c813611785c35d5419c36e6902
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-06 19:29:59 +0900

    Appbar에 IconButton 추가하고 print로 콘솔창에 문자찍기

--- lib/main.dart
+++ lib/main.dart
@@ -7,137 +7,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BBANTO',
-      home: Grade(),
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('BBANTO'),
-        backgroundColor: Colors.amber[700],
+        title: Text('Appbar icon menu'),
         centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/1.jpg'),
-                radius: 60,
-              ),
-            ),
-            Divider(
-              height: 60,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'BBANTO',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'BBANTO',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'BBANTO POWER LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '14',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'using lightsaber',
-                  style: TextStyle(fontSize: 16, letterSpacing: 1),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'face hero tottoo',
-                  style: TextStyle(fontSize: 16, letterSpacing: 1),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'fire flames',
-                  style: TextStyle(fontSize: 16, letterSpacing: 1),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/2.jpg'),
-                radius: 40,
-                backgroundColor: Colors.amber[800],
-              ),
-            )
-          ],
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('menu button is clicked');
+          },
         ),
       ),
     );
