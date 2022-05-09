ff38ca5a859ebb30244f897402c767793160b195
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-09 19:02:52 +0900

    Navigator push pop MaterialPageRoute

--- lib/main.dart
+++ lib/main.dart
@@ -11,131 +11,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Appbar',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
+      home: FirstPage(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the Second page'),
+          onPressed: () {
+            Navigator.push(context2,
+                MaterialPageRoute(builder: (context) => SecondPage()));
+          },
+        ),
+      ),
+    );
+  }
+}
 
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Toast message'),
-        centerTitle: true,
+        title: Text('Second Page'),
       ),
       body: Center(
-        child: FlatButton(
+        child: RaisedButton(
+          child: Text('Go to the First page'),
           onPressed: () {
-            flutterToast();
+            Navigator.pop(context);
           },
-          child: Text('Toast'),
-          color: Colors.blue,
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/1.jpg'),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/2.jpg'),
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage('assets/2.jpg'),
-                // ),
-              ],
-              //accountName,accountEmail -> required default
-              accountName: Text('GGAM'),
-              accountEmail: Text('123@naver.com'),
-              onDetailsPressed: () {
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () {
-                print('Home is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('settings'),
-              onTap: () {
-                print('settings is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('Q & A'),
-              onTap: () {
-                print('Q & A is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
         ),
       ),
     );
   }
-}
-
-class MySnackBar extends StatelessWidget {
-  const MySnackBar({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                'Hello',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white),
-              ),
-              backgroundColor: Colors.teal,
-              duration: Duration(milliseconds: 1000),
-            ),
-          );
-        },
-        child: Text('Show me'),
-      ),
-    );
-  }
-}
-
-void flutterToast() {
-  Fluttertoast.showToast(
-      msg: 'Flutter',
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.redAccent,
-      fontSize: 20,
-      textColor: Colors.white,
-      toastLength: Toast.LENGTH_SHORT);
 }
