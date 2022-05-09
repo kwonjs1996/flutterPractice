377896200f49745bc891cffbca56f8e7fa74e6bd
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-09 20:48:32 +0900

    숙제 해결

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,25 @@ class MyApp extends StatelessWidget {
 
   @override
   //BuildContext
+  //Flutter 트리 구조상 MaterialApp 안에서 실행되어야 하기 때문에 builder함수를 통하여 안에 새로 추가를 하거나
+  //새로운 Class(=MyPage)를 MaterialApp안에 추가하여 그 안에서 페이지를 라우트 시킨다.
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: ScreenA()));
-          },
-          child: Text('Go to ScreenA'),
-        ),
+    return MaterialApp(home: MyPage());
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ScreenA()));
+        },
+        child: Text('Go to ScreenA'),
       ),
     );
   }
