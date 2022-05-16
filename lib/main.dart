67529b8880a003feac03ffc6a0d6d2dd0aea0b60
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-16 13:03:15 +0900

    loop 로또번호 추첨기

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,77 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'login_app/login.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Login app',
+//       theme: ThemeData(primarySwatch: Colors.grey),
+//       home: LogIn(),
+//     );
+//   }
+// }
 
-import 'login_app/login.dart';
+import 'dart:math';
 
-void main() {
-  runApp(MyApp());
+List<int> lottoNumber() {
+  var random = Random();
+  List<int> lottoList = [];
+  var num;
+
+  for (int i = 0; i < 6; i++) {
+    num = random.nextInt(45) + 1;
+    lottoList.add(num);
+  }
+
+  print('당첨번호');
+  print(lottoList);
+
+  return lottoList;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+List<int> myNumber() {
+  var random = Random();
+  List<int> myList = [];
+  var num;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login app',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      home: LogIn(),
-    );
+  for (int i = 0; i < 6; i++) {
+    num = random.nextInt(45) + 1;
+    myList.add(num);
   }
+
+  print('내 추첨번호');
+  print(myList);
+
+  return myList;
+}
+
+void checkNumber(lottoList, myList) {
+  int match = 0;
+
+  for (int lotto in lottoList) {
+    for (int myNum in myList) {
+      if (lotto == myNum) {
+        match++;
+        print('당첨번호: $myNum');
+      }
+      //print('로또번호 = $lotto');
+      //print('내 추첨번호 = $myNum');
+    }
+  }
+  print('$match개의 당첨번호가 있습니다.');
+}
+
+void main() {
+  List<int> lottoFinal = lottoNumber();
+  List<int> myFinal = myNumber();
+
+  checkNumber(lottoFinal, myFinal);
 }
